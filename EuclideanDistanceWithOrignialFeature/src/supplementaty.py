b858efef8a16a119d72a6a54cb97d14262c7dfdc
chinabdd <2714065684@qq.com>
2016-03-26 16:16:22 +0800

    add dataset selection

--- EuclideanDistanceWithOrignialFeature/src/supplementaty.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-f1=open('result.py','r+')
-f2=open('result2.py','w+')
-for i in f1:
-        s='    '+i
-        f2.write(s)
-f2.close()
--- hammingDistanceWithCompressed feature/src_as_backup/plot.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-import result
-import numpy as np
-
-threshold=np.arange(1600,2800,2)
-result.result(threshold,"conv3")
- 
